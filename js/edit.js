56dfbe52f8c180d838f9b2c8e96cd7ae7d98d95f
naruto1031 <naruto0614@au.com>
2023-02-22 16:55:47 +0900

    アイコン画像を追加し、jsを修正

--- js/edit.js
+++ js/edit.js
@@ -406,6 +406,58 @@ function change_preview() {
 
 }
 
+//フォント変更
+$(function () {
+    $("[name='fontFamily']").change(function () {
+        var font = $(this).val();
+
+        $(".main-edit-content").css("width", "fit-content");
+        // debugger;
+        G_current_text = $(`#${G_current_focus}`).find(".text");
+        G_current_text.css("fontFamily", font);
+
+        fitty('.fit', {
+            minSize: 12,
+            maxSize: 100,
+        });
+
+        $(".main-edit-content").css("width", "fit-content");
+        var resize_width = document.querySelectorAll(".fit");
+        var px_width = [];
+
+        $(".main-edit-content").css("width", (index, _) => {
+            px_width[index] = resize_width[index].getBoundingClientRect();
+            return `${px_width[index].width}px`;
+        });
+
+    });
+
+});
+
+//テキスト縦横変更
+$(function () {
+    $("[name='writingMode']").change(function () {
+        var WTorien = $(this).val();
+        G_current_text = $(`#${G_current_focus}`).find(".text");
+        G_current_width = $(`#${G_current_focus}`).css("height");
+        G_current_height = $(`#${G_current_focus}`).css("width");
+
+        $(`#${G_current_focus}`).css("width", G_current_width);
+        $(`#${G_current_focus}`).css("height", G_current_height);
+
+        G_current_text.css("writing-mode", WTorien);
+        G_current_text.css("-webkit-writing-mode", WTorien);
+        G_current_text.css("-ms-writing-mode", WTorien);
+
+        G_writing_mode = G_current_text.css("writing-mode");
+
+        fitty('.fit', {
+            minSize: 12,
+            maxSize: 100,
+        });
+    });
+});
+
 const remove_button = document.getElementById("remove");
 remove_button.addEventListener("click", remove_element)
 /**
